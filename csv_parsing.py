815dab0db8895deecbe763d1cb93f8b53b13350c
Sid <sid666dmz@gmail.com>
2023-11-13 21:58:44 +0800

    fix: 🐛 wrong src

--- csv_parsing.py
+++ csv_parsing.py
@@ -195,7 +195,7 @@ def normalize_report_name(report_dir: str, pp: str, mp: str):
 def drs_upload(path_prefix: str, vendor: str, report_dir: str, pp: str, mp: str):
     id = f"{pp}_{mp}"
     cmd = 'seqslab datahub upload --src "{0}" --dst {3}/{1}/ --workspace vghtpe > {2}/{1}_tmp.json'.format(
-        f"{path_prefix}/{vendor}/{report_dir}/*",
+        f"{report_dir}/*",
         f"{id}",
         f"{path_prefix}/{vendor}",
         f"{vendor}")
